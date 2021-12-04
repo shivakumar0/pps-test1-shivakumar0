5c62f67e4f5165488252d2c7f082ccc6ab5bec35
shivakumar0 <94587715+shivakumar0@users.noreply.github.com>
2021-12-04 10:02:12 +0000

    complete

--- p2original.c
+++ p2original.c
@@ -1,8 +1,28 @@
-#include<sstdio.h>
+#include<stdio.h>
+int intput()
+{ 
+  int a;
+  printf("enter any number");
+  scanf("%d ",&a);
+  return a;
+}
+int add (int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+int output(int sum)
+{
+  printf("sum is %d:",sum);
+}
 int main()
 {
-  int (" a,b,sum")
-  printf(" enter 2 num
-  
-  return 0;
+  int main()
 }
+int a,b,sum;
+a=input();
+b=input();
+sum=add(a,b);
+output(sum);
+return 0;
