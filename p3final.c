6e9579d55c58eb432e8dc1ad9b97cbf50db0818d
shivakumar0 <94587715+shivakumar0@users.noreply.github.com>
2022-01-21 05:37:40 +0000

    p1f

---
+++ p3final.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void input(int*a,int*b)
+{
+  printf("enter numbers\n");
+  scanf("%d%d",a,b);
+}
+  void add(inta,intb,int*sum)
+{
+  *sum=a+b;
+}
+void output(inta,intb,int sum)
+{
+  printf("sum of %d+%d is %d",a,b,sum);
+}
+int main()
+{
+int a,b,sum;
+input(&a,&b);
+add(a,b,&sum);
+output(a,b,sum);
+return 0;
+
+}
